78e7c5bcf5784077f72740566375cf37b34f5e0d
andriikovall <anderkoval3@gmail.com>
2025-05-29 16:24:25 +0200

    feat: init

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <div className="container">
+      <div className="card">
+        <p className="text-uppercase">Some text inside the card</p>
+      </div>
+    </div>
+  );
+}
+
+export default App;
