f869547a575fe2354f06283acd7dfa5c91889254
andriikovall <anderkoval3@gmail.com>
2025-05-31 15:35:28 +0200

    feat: default folder navigation

--- src/lib/navigation/RootNavigation.tsx
+++ src/lib/navigation/RootNavigation.tsx
@@ -1,11 +1,12 @@
-import { Route, Routes } from "react-router";
+import { Navigate, Route, Routes } from "react-router";
 import { MainRoute } from "./routes";
 import { EmailPreview } from "./routes/MainRoute/routes/EmailPreview";
+import { DEFAULT_FOLDER_SLUG } from "../constants/folders";
 
 export const RootNavigation = () => {
   return (
     <Routes>
-      <Route path="/" Component={MainRoute} />
+      <Route path="/" element={<Navigate to={`/folders/${DEFAULT_FOLDER_SLUG}`} />} />
       <Route path="/folders/:folderSlug" Component={MainRoute}>
         <Route path="email/:emailId?" Component={EmailPreview} />
       </Route>
