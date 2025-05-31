3eb6a48cbc4dff18ae09d662fe623d9f79ba62a4
andriikovall <anderkoval3@gmail.com>
2025-05-31 15:37:12 +0200

    feat: added default path navigation

--- src/lib/navigation/RootNavigation.tsx
+++ src/lib/navigation/RootNavigation.tsx
@@ -4,12 +4,14 @@ import { EmailPreview } from "./routes/MainRoute/routes/EmailPreview";
 import { DEFAULT_FOLDER_SLUG } from "../constants/folders";
 
 export const RootNavigation = () => {
+  const defaultPath = `/folders/${DEFAULT_FOLDER_SLUG}`;
   return (
     <Routes>
-      <Route path="/" element={<Navigate to={`/folders/${DEFAULT_FOLDER_SLUG}`} />} />
+      <Route path="/" element={<Navigate to={defaultPath} />} />
       <Route path="/folders/:folderSlug" Component={MainRoute}>
         <Route path="email/:emailId?" Component={EmailPreview} />
       </Route>
+      <Route path="*" element={<Navigate to={defaultPath} />} />
     </Routes>
   );
 };
